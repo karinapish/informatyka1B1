37f3a102340c75b2e7754dc110ad9e651b7af06f
Karyna Pishchyk <karinapishchyk@gmail.com>
2022-03-15 14:12:12 +0100

    zrobione nww

---
+++ nww  nwd/nww  nwd.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Wprowadz pierwszą liczbę";
+	int a;
+	cin >> a;
+	cout << "Wprowadz drugą liczbę";
+	int b;
+	cin >> b;
+	while (a != b)
+	{
+		if (a < b)
+			b = b - a;
+		else  a = a - b;
+	}
+	cout << a;
+
+	return;
+}
+
