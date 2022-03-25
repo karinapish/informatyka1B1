f238ed476892f6cff9463b1db7e2c628209342c3
karinapish <100408824+karinapish@users.noreply.github.com>
2022-03-25 11:52:57 +0100

    Update nww  nwd.cpp

--- nww  nwd/nww  nwd.cpp
+++ nww  nwd/nww  nwd.cpp
@@ -3,20 +3,23 @@ using namespace std;
 
 int main()
 {
-	cout << "Wprowadz pierwszą liczbę";
+	cout << "Wprowadz pierwszą liczbę: ";
 	int a;
 	cin >> a;
-	cout << "Wprowadz drugą liczbę";
+	cout << "Wprowadz drugą liczbę: ";
 	int b;
 	cin >> b;
+	int iloczyn = a * b;
 	while (a != b)
 	{
 		if (a < b)
 			b = b - a;
 		else  a = a - b;
-	}
-	cout << a;
+	}                         // obliczony nwd
+	int nww = iloczyn / a;
+	cout << nww;              // obliczony nww
 
-	return;
+	return 0;
+	
 }
 
