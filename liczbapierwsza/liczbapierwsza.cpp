a94edaee449338972b70e6560f099675e6963583
PatrycjaHolda <isia0807@gmail.com>
2022-04-05 14:23:46 +0200

    liczbypierwsze

--- liczbapierwsza/liczbapierwsza.cpp
+++ liczbapierwsza/liczbapierwsza.cpp
@@ -2,19 +2,30 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	int n;
+	cout << "Ile liczb pierwszych chcesz wyswietlic:";
+	cin >> n;
+	int lp = 0;
+	int p = 2;
+	long l1;
+	bool m = true;
+	while (lp < n)
+	{
+		m = true;
+		for(int d=2; d <=p-1; d++){
+			if (p % d == 0) {
+				m = false;
+			}
+		}
+		if (m == true) {
+			cout << p<<" ";
+			lp = lp + 1;
+		}
+		p = p + 1;
+		
+	}
+	return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
